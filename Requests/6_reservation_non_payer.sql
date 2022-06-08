59ec6885355ca09cebdd55818d82200ec1c022ee
Rojotiana Nomena <hei.nomena@gmail.com>
2022-06-09 01:54:55 +0300

    feat: request 6

--- Requests/6_reservation_non_payer.sql
+++ Requests/6_reservation_non_payer.sql
@@ -1,4 +1,5 @@
-select nom_client , prenom_client , type , tarif , place, montant_paye from client , offre ,reserver 
+select nom_client, prenom_client, type, tarif, montant_paye 
+from client, offre, reserver 
 where client.id_client = reserver.id_client 
 and offre.id_offre = reserver.id_offre 
-and montant_paye*reserver.place < tarif*reserver.place ;
+and montant_paye < tarif ;
