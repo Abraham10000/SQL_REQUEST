d72d6a9a2e20dee685ff717b0d9efe294d7c8e73
Rojotiana Nomena <hei.nomena@gmail.com>
2022-06-21 13:11:52 +0300

    fix: 18_tous_les_voyages_semaine

--- Requests/18_tous_les_voyage_semaine.sql
+++ Requests/18_tous_les_voyage_semaine.sql
@@ -1,3 +1,3 @@
 -- tous les voyages durant un jour
-select * from voyage where date_part('day', jour_depart) <= date_part('day', current_date)
-and date_part('day', jour_depart) > date_part('day', current_date)-7;
+select * from voyage where jour_depart <= current_date
+and jour_depart > current_date-7;
